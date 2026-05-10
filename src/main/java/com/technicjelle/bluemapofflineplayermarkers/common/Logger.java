3d633fbb5c2d513b913c0b9aa7219ba090d58fa9
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2026-05-11 00:09:47 +0200

    Added a doc-comment to the Logger interface

--- src/main/java/com/technicjelle/bluemapofflineplayermarkers/common/Logger.java
+++ src/main/java/com/technicjelle/bluemapofflineplayermarkers/common/Logger.java
@@ -1,5 +1,11 @@
 package com.technicjelle.bluemapofflineplayermarkers.common;
 
+/// You can implement this logger for your platform.
+///
+/// But if your platform offers a [java.util.logging.Logger], consider using [JavaUtilLogger].
+///
+/// You can also use [BMNLogger], which will "hijack" BlueMap's own logging system, through [com.technicjelle.BMUtils.BMNative.BMNLogger].<br>
+/// This has the added benefit of logging the fine details into BlueMap's `debug.log` file, so they don't disappear into the ether.
 public interface Logger {
 	void error(String message);
 	void error(String message, Throwable throwable);
